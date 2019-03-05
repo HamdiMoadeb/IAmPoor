745652951251cbb380aea5f17932292b033fac58
MacBookPro <macbookpro@macbook-pro-de-macbookpro.local>
2019-03-05 10:23:02 +0100

    Initial Commit

---
+++ IAmPoor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by MacBookPro on 3/5/19.
+//  Copyright © 2019 Saphir Consult. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
